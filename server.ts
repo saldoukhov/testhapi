c49c1dffe5b98e554985b2eedf20e34de7a830f8
Sergey Aldoukhov <saldoukhov@gmail.com>
2016-06-28 17:57:04 -0700

    Init

---
+++ server.ts
@@ -0,0 +1,15 @@
+///<reference path="./typings/main.d.ts"/>
+
+'use strict';
+
+import * as Hapi from "hapi";
+
+const server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
